75e16b2901d449503e72eb20fd679212aa9b3ba7
Ann-Fox <fx8565@mail.ru>
2024-12-14 16:41:47 +0500

    feat: add burger menu

--- js/script.js
+++ js/script.js
@@ -36,6 +36,29 @@ if (menuLinks.length > 0) {
    }
 }
 
+// Меню бургер
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+   document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//  if (!event.target.matches('.dropbtn')) {
+
+//    var dropdowns = document.getElementsByClassName("dropdown-content");
+//    var i;
+//    for (i = 0; i < dropdowns.length; i++) {
+//      var openDropdown = dropdowns[i];
+//      if (openDropdown.classList.contains('show')) {
+//        openDropdown.classList.remove('show');
+//      }
+//    }
+//  }
+// }
+
 // Initialize Swiper
 new Swiper(".mySwiper", {
     cssMode: true,
